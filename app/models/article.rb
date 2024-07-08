872620744bf2ed085dcafb5d404c6ab19260f0a5
長野汐央里 <>
2024-07-08 23:40:56 +0900

    has_many, belongs_toを記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -26,6 +26,8 @@ class Article < ApplicationRecord
 
   validate :validate_title_and_content_length
 
+  belongs_to :user
+
   def display_created_at
     I18n.l(created_at, format: :default)
   end
