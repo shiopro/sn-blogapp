fcaed49eb10eb97bcf4a3b0db077d18f40fa9df0
長野汐央里 <>
2024-08-30 20:36:31 +0900

    action textの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -24,10 +24,6 @@ class Article < ApplicationRecord
   validates :title, format: { with: /\A(?!\@)/ }
 
   validates :content, presence: true
-  validates :content, length: { minimum: 10 }
-  validates :content, uniqueness: true
-
-  validate :validate_title_and_content_length
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -44,12 +40,4 @@ class Article < ApplicationRecord
   def like_count
     likes.count
   end
-
-  private
-    def validate_title_and_content_length
-      char_count = self.title.length + self.content.length
-      unless char_count > 100
-        errors.add(:content, '100文字以上で')
-      end
-    end
 end
